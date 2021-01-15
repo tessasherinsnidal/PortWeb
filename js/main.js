1f31e7420a5fea21dd6884a0cfec1fb19d57ac13
tessasherinsnidal <tessasherinsnidal@icloud.com>
2021-01-15 14:21:13 -0500

    update links and make sure all links work :D

--- js/main.js
+++ js/main.js
@@ -161,3 +161,36 @@
 		document.getElementById("mySidebar").style.width = "0";
 		document.getElementById("main").style.marginRight= "0";
 	}
+
+///*** On Click Change Image ***///
+
+
+    var gStorage = {};
+
+	function toogle(anImage, anAltSrcArr) {
+		if (typeof(anImage) === "undefined" || typeof(anAltSrcArr) === "undefined" || anAltSrcArr.length === 0) {
+			return;
+		}
+
+		var id = anImage.id;
+		var oldSrc = anImage.src;
+
+		if (typeof(gStorage[id]) === "undefined") {
+			gStorage[id] = {
+				'id': id,
+				'origSrc': oldSrc,
+				'i': 0
+			};
+		}
+
+		gStorage[id].i += 1;
+		if (gStorage[id].i > anAltSrcArr.length) {
+			gStorage[id].i = 0;
+		}
+
+		if (gStorage[id].i === 0) {
+			anImage.src = gStorage[id].origSrc;
+		} else {
+			anImage.src = anAltSrcArr[gStorage[id].i - 1];
+		}
+	}
