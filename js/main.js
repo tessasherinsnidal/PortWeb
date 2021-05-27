e996fa42bed36e4fc579f3e509b2dde41883b7ad
tessasherinsnidal <tessasherinsnidal@icloud.com>
2021-05-27 10:29:02 -0400

    nrc

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@
 
 //***Countdown timer***//
 	// Set the date to count down to
-	var countDownDate = new Date("Mar 1, 2021 00:00:00").getTime();
+	var countDownDate = new Date("June 31, 2021 00:00:00").getTime();
 
 	// Update the count down every 1 second
 	var x = setInterval(function() {
