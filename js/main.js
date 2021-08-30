4937f50dd353951acb4e057d0cce03316c1244dc
tessasherinsnidal <tessasherinsnidal@icloud.com>
2021-08-30 12:28:57 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -59,7 +59,8 @@
 
 //***Countdown timer***//
 	// Set the date to count down to
-	var countDownDate = new Date("August 31, 2021 00:00:00").getTime();
+	var countDownDate = new Date("September 30
+, 2021 00:00:00").getTime();
 
 	// Update the count down every 1 second
 	var x = setInterval(function() {
